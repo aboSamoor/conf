d0084e8bf02065e55d658d8b0e57fcacf7d56ec9
Rami Al-Rfou <rmyeid@gmail.com>
2013-03-14 06:27:32 -0400

    add pylint,pep8 tools and update template and vimrc

---
+++ vim/skel/template_test.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""test_template.py: Unit Test for a module."""
+
+import unittest
+
+__author__ = "Rami Al-Rfou"
+__email__ = "rmyeid@gmail.com"
+
+class TestClass(unittest.TestCase):
+  def setUp(self):
+    pass
+
+  def tearDown(self):
+    pass
+
+
+if __name__ == "__main__":
+  unittest.main()   
